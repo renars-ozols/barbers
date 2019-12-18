d813cd9074cf750eabe5f41d1c057324973c9014
renars1988 <renarsozols@gmail.com>
2019-12-18 05:23:19 +0000

    added fixed navigation back to header component

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
 import TopNavigation from '../top-navigation/top-navigation'
-//import FixedNav from '../fixed-nav/fixed-nav'
+import FixedNav from '../fixed-nav/fixed-nav'
 
 const Header = () => (
   <header>
     <TopNavigation />
+    <FixedNav />
   </header>
 )
 
