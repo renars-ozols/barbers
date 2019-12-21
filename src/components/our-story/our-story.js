0de124ec8b01f50f4b578a086b58eda0ac52236b
renars1988 <renarsozols@gmail.com>
2019-12-21 05:22:14 +0000

    removed react-reveal from project

--- src/components/our-story/our-story.js
+++ src/components/our-story/our-story.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { useStaticQuery, graphql } from 'gatsby'
-import Fade from 'react-reveal/Fade'
 
 import Heading from '../heading/heading'
 import { Background, InnerWrapper, Paragraph } from './our-story.styles'
@@ -24,36 +23,32 @@ const OurStory = () => {
   return (
     <Background Tag="section" fluid={imageStack}>
       <InnerWrapper>
-        <Fade top>
-          <Heading
-            h2
-            center
-            noMarginTop
-            css={`
-              color: ${({ theme }) => theme.colors.greyLight1};
-              margin-bottom: 2rem;
-            `}
-          >
-            Our Story
-          </Heading>
-        </Fade>
-        <Fade left>
-          <Paragraph>
-            Aliquam malesuada bibendum arcu vitae elementum curabitur vitae.
-            Nulla aliquet porttitor lacus luctus accumsan tortor. Risus at
-            ultrices mi tempus imperdiet nulla malesuada pellentesque.
-            Pellentesque dignissim enim sit amet venenatis. Venenatis urna
-            cursus eget nunc scelerisque viverra. Ultrices eros in cursus turpis
-            massa tincidunt dui ut. Ultricies mi quis hendrerit dolor magna eget
-            est lorem. Aliquam vestibulum morbi blandit cursus risus at ultrices
-            mi. Nunc vel risus commodo viverra maecenas accumsan lacus. Pharetra
-            sit amet aliquam id. Donec pretium vulputate sapien nec sagittis
-            aliquam malesuada bibendum arcu. Rhoncus est pellentesque elit
-            ullamcorper dignissim. Praesent elementum facilisis leo vel
-            fringilla est. Tellus cras adipiscing enim eu turpis egestas pretium
-            aenean. Ante in nibh mauris cursus mattis molestie.
-          </Paragraph>
-        </Fade>
+        <Heading
+          h2
+          center
+          noMarginTop
+          css={`
+            color: ${({ theme }) => theme.colors.greyLight1};
+            margin-bottom: 2rem;
+          `}
+        >
+          Our Story
+        </Heading>
+        <Paragraph>
+          Aliquam malesuada bibendum arcu vitae elementum curabitur vitae. Nulla
+          aliquet porttitor lacus luctus accumsan tortor. Risus at ultrices mi
+          tempus imperdiet nulla malesuada pellentesque. Pellentesque dignissim
+          enim sit amet venenatis. Venenatis urna cursus eget nunc scelerisque
+          viverra. Ultrices eros in cursus turpis massa tincidunt dui ut.
+          Ultricies mi quis hendrerit dolor magna eget est lorem. Aliquam
+          vestibulum morbi blandit cursus risus at ultrices mi. Nunc vel risus
+          commodo viverra maecenas accumsan lacus. Pharetra sit amet aliquam id.
+          Donec pretium vulputate sapien nec sagittis aliquam malesuada bibendum
+          arcu. Rhoncus est pellentesque elit ullamcorper dignissim. Praesent
+          elementum facilisis leo vel fringilla est. Tellus cras adipiscing enim
+          eu turpis egestas pretium aenean. Ante in nibh mauris cursus mattis
+          molestie.
+        </Paragraph>
       </InnerWrapper>
     </Background>
   )
