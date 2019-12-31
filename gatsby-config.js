e1de90e2da49b603115d80dd553770695fc1d9fe
renars1988 <renarsozols@gmail.com>
2019-12-31 08:58:08 +0000

    added preact plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,6 +34,7 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-styled-components`,
+    `gatsby-plugin-preact`,
     {
       resolve: 'gatsby-plugin-react-svg',
       options: {
