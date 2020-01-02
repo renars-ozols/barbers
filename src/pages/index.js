32acddd2f5347452b4b4e4008cbc2284078851fd
renars1988 <renarsozols@gmail.com>
2020-01-02 16:35:30 +0000

    trying to dedect performance issue

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 import Layout from '../components/layouts/main'
 import Content from '../components/content'
-//import Hero from '../components/hero/hero'
+import Hero from '../components/hero/hero'
 import Box from '../components/box/box'
 import WhatWeDo from '../components/what-we-do/what-we-do'
-//import OurStory from '../components/our-story/our-story'
+import OurStory from '../components/our-story/our-story'
 import PriceList from '../components/price-list/price-list'
 import Gallery from '../components/gallery/gallery'
 import SEO from '../components/seo/seo'
@@ -13,14 +13,14 @@ import SEO from '../components/seo/seo'
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <Gallery />
+    <Hero />
     <Box>
-      <WhatWeDo />
+      <Content />
     </Box>
-    <WhatWeDo />
+    <OurStory />
     <Box>
       <PriceList />
-      <Gallery />
+      <Content />
     </Box>
   </Layout>
 )
