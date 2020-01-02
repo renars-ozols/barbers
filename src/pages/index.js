84ea3c945d85201162b4ce28b313b10263c60e57
renars1988 <renarsozols@gmail.com>
2020-01-02 16:22:58 +0000

    removed hero and our story components from index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 import Layout from '../components/layouts/main'
 import Content from '../components/content'
-import Hero from '../components/hero/hero'
+//import Hero from '../components/hero/hero'
 import Box from '../components/box/box'
 import WhatWeDo from '../components/what-we-do/what-we-do'
-import OurStory from '../components/our-story/our-story'
+//import OurStory from '../components/our-story/our-story'
 import PriceList from '../components/price-list/price-list'
 import Gallery from '../components/gallery/gallery'
 import SEO from '../components/seo/seo'
@@ -13,11 +13,11 @@ import SEO from '../components/seo/seo'
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <Hero />
+    <Gallery />
     <Box>
       <WhatWeDo />
     </Box>
-    <OurStory />
+    <WhatWeDo />
     <Box>
       <PriceList />
       <Gallery />
